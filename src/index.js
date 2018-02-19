5824560e28469d32a53eae63a7848b338d24f0f8
Katsiaryna Mashokha <katsiarynamashokha@gmail.com>
2018-02-19 11:51:46 -0800

    refactor: swap BrowserRouter with HashRouter to deploy to Githib pages

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-    <BrowserRouter><App /></BrowserRouter>, 
+    <HashRouter><App /></HashRouter>, 
     document.getElementById('root'));
 registerServiceWorker();
